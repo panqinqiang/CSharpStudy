411b4779810f533050477e507ca2a870457c0199
panqinqiang <qinqiangpan@gmail.com>
2023-10-22 17:27:57 +0800

    O:MVC练习

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Demo1()  //Action方法，操作方法
+        {
+            //return View();
+            Person p1 = new Person("杨中科242", true, DateTime.Now);
+            return View(p1);
+        }
+    }
+}
---
+++ WebApplication1/Models/Person.cs
@@ -0,0 +1,4 @@
+﻿namespace WebApplication1.Models
+{
+    public record Person(string Name, bool IsVIP, DateTime CreatedDateTime); 
+}
